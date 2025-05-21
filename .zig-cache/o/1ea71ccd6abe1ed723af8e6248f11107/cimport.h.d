ca03c610c9ce19e9804f6ad47b91e65729b76ec0
Ben Landrette <128928536+BendyLand@users.noreply.github.com>
2025-05-20 20:32:37 -0400

    Initial commit

---
+++ .zig-cache/o/1ea71ccd6abe1ed723af8e6248f11107/cimport.h.d
@@ -0,0 +1,19 @@
+cimport.o: \
+  /home/ben-landrette/code/do/.zig-cache/o/1ea71ccd6abe1ed723af8e6248f11107/cimport.h \
+  /usr/include/x86_64-linux-gnu/sys/stat.h /usr/include/features.h \
+  /usr/include/features-time64.h \
+  /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+  /usr/include/x86_64-linux-gnu/bits/timesize.h \
+  /usr/include/stdc-predef.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+  /usr/include/x86_64-linux-gnu/bits/long-double.h \
+  /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+  /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+  /usr/include/x86_64-linux-gnu/bits/types.h \
+  /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+  /usr/include/x86_64-linux-gnu/bits/time64.h \
+  /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+  /usr/include/x86_64-linux-gnu/bits/endian.h \
+  /usr/include/x86_64-linux-gnu/bits/endianness.h \
+  /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+  /usr/include/x86_64-linux-gnu/bits/stat.h \
+  /usr/include/x86_64-linux-gnu/bits/struct_stat.h
